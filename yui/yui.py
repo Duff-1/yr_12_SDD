855d05f7cbb16d91956f84282085e130d1fed547
Duff-1 <jimbo4iruu@gmail.com>
2024-04-08 13:54:00 +1000

    Create yui.py

---
+++ yui/yui.py
@@ -0,0 +1,19 @@
+int__ = 1
+string__ = "wow"
+array = [1,2,3]
+
+y = int(input("number1: "))
+
+z = int(input("number2: "))
+
+
+temp = 0
+
+for thing in array:
+    temp += thing
+    print(temp)
+
+
+print(array[0])
+print(array[1])
+print(array[2])
