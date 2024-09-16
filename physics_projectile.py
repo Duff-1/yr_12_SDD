1e1ce4b5ff1a90cc2e2d1bae694c2477e0be5ace
Duff-1 <jimbo4iruu@gmail.com>
2024-09-16 14:50:04 +1000

    Update physics_projectile.py

--- physics_projectile.py
+++ physics_projectile.py
@@ -14,8 +14,8 @@ FONT = pygame.font.SysFont('Arial', 20)
 # Game Setup
 FPS = 60
 fpsClock = pygame.time.Clock()
-WINDOW_HEIGHT = 400
-WINDOW_WIDTH = 400
+WINDOW_HEIGHT = 500
+WINDOW_WIDTH = 800
 
 WINDOW = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
 pygame.display.set_caption('Game')
@@ -23,7 +23,7 @@ pygame.display.set_caption('Game')
 def update():
     fpsClock.tick(FPS)
     WINDOW.fill(BACKGROUND)
-    #pygame.draw.circle(WINDOW,BLUE,(WINDOW_HEIGHT/2,WINDOW_WIDTH/2),30.0,2)
+    createArc(200,60,9.8)
     pygame.display.flip()
 
 def createArc(total_velocity, angle, gravity):
@@ -32,14 +32,16 @@ def createArc(total_velocity, angle, gravity):
     xVelocity = math.cos(math.radians(angle))*total_velocity
     time = math.sqrt((2*yVelcoity)/gravity)
     xEnd = (xVelocity*time)
-    xPoint = (xEnd,0)
+    xPoint = (xEnd,WINDOW_HEIGHT)
     yMax = (yVelcoity*(time/2))-((gravity*(time*time))/2)
-    yPoint = ((xEnd/2),yMax)
+    yPoint = ((xEnd/2),WINDOW_HEIGHT-yMax)
+
 
     #display points
-    pygame.draw.circle(WINDOW,BLUE,(0,0),2,2) #start
-    pygame.draw.circle(WINDOW,BLUE,yPoint,2,2) #max height
-    pygame.draw.circle(WINDOW,BLUE,xPoint,2,2) #end
+    pygame.draw.circle(WINDOW,BLUE,(0,WINDOW_HEIGHT),5,3) #start
+    pygame.draw.circle(WINDOW,BLUE,yPoint,5,3) #max height
+    pygame.draw.circle(WINDOW,BLUE,xPoint,5,3) #end
+
 
 
     
@@ -52,7 +54,6 @@ def main():
             if event.type == QUIT :
                 pygame.quit()
                 sys.exit()
-        createArc(100,45,9.8)
         update()
 
 
